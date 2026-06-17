8fdb2a9da524a6c55369c9b4eba8f6833e797d00
Dromilkins <amelchenkovova0@gmail.com>
2026-06-17 16:20:23 +0300

    Add wsgi.py and fix settings

--- myshop/settings.py
+++ myshop/settings.py
@@ -6,10 +6,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ваш-ключ-для-разработки')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
